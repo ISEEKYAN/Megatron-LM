06d9d1adee5d56a459ffcf15e173d21ff4c74eb1
Yan Bai <bayan@nvidia.com>
2026-07-28 19:37:06 -0700

    Fuse token permutation for EP chunks

--- experimental/lite/megatron/lite/model/qwen3_moe/lite/model.py
+++ experimental/lite/megatron/lite/model/qwen3_moe/lite/model.py
@@ -86,6 +86,7 @@ class MoELayer(nn.Module):
                     config.hidden_size,
                     ps,
                     use_deepep=use_deepep,
+                    moe_permute_fusion=True,
                     buffer_slot=("ep_chunk_overlap", "forward", layer_slot, idx),
                 )
                 for idx in range(num_chunks_ep_a2a_overlap)
